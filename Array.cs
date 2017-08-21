7d17318364cd05aeb75c926c0770df8b9cc2149d
Омиржан Нуртлеу Абылхатович <>
2017-08-21 15:58:40 +0600

    HW6

---
+++ Array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW
+{
+    public class Array
+    {
+        private int[] array = new int[20];
+        public int this[int i]
+        {
+            get
+            {
+                return array[i];
+            }
+
+            set
+            {
+                array[i] = (value * value);
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace HW
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Array array = new Array();
+            for (int i=0; i<10; i++)
+            {
+                array[i] = i;
+            }
+
+            for (int i=0; i<10; ++i)
+            {
+                WriteLine("№{0}={1}",i,array[i]);
+            }
+            ReadLine();
+        }
+    }
+}
